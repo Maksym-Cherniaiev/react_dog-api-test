958f9bf7cb545f5e1597a7df4434b3bd9d910d28
Maksym <likeran2@gmail.com>
2019-06-07 01:16:06 +0300

    Updates

---
+++ precache-manifest.b8631c0d82960d1ca7c5aba45a5eb058.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79ca3277d5e945bbf84c651bb5e82a6a",
+    "url": "/react_dog-api-test/index.html"
+  },
+  {
+    "revision": "18b72774a247c98d0686",
+    "url": "/react_dog-api-test/static/js/2.61e80a56.chunk.js"
+  },
+  {
+    "revision": "a1aedf11e30251a066cb",
+    "url": "/react_dog-api-test/static/js/main.23e7753a.chunk.js"
+  },
+  {
+    "revision": "e7b181b5c69b750467b6",
+    "url": "/react_dog-api-test/static/js/runtime~main.05facc10.js"
+  }
+]);
